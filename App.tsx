a5c02153c90d60f928e217ace5daea35c8153ef1
Salih UĞUR <salihugur46@gmail.com>
2025-04-26 23:15:26 +0300

    try husky

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button, Text, View } from 'react-native';
+
 import { StyleSheet, UnistylesRuntime, withUnistyles } from 'react-native-unistyles';
 
 const UniButton = withUnistyles(Button, (theme) => ({
