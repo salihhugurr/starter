6614c240b19ce0eb227af14bbb043249a63fdd9b
Salih UĞUR <salihugur46@gmail.com>
2025-04-26 23:26:13 +0300

    husky added

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  arrowParens: 'always',
+  arrowParens: "always",
   bracketSameLine: true,
   singleQuote: true,
   semi: true,
   printWidth: 100,
   tabWidth: 2,
-  trailingComma: 'all',
+  trailingComma: "all",
   bracketSpacing: true,
 };
